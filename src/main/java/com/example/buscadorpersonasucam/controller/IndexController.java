193dbd24330885c57dcb3657a603f8634786a87b
unknown <Jose ngel@MUMONASLDTIC209.vdi.ucam.edu>
2023-03-28 13:52:51 +0200

    Cambios para elastic

--- src/main/java/com/example/buscadorpersonasucam/controller/IndexController.java
+++ src/main/java/com/example/buscadorpersonasucam/controller/IndexController.java
@@ -1,5 +1,7 @@
 package com.example.buscadorpersonasucam.controller;
 
+import com.example.buscadorpersonasucam.database.entity.PersonaElastic;
+import org.elasticsearch.search.SearchHits;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -30,87 +32,23 @@ public class IndexController {
 
         //todo Aqui hace el pedido al backend con la id que se le pasa {id}
         //devuelde una persona completa con todos los datos para usarlos en el frontend
-        List<Persona> personas = new ArrayList<>();
-        personas.add(new Persona(1, "Jose Ángel", "Alcolea Arques"));
-        personas.add(new Persona(2, "Jose Manuel", "Martín Melero"));
-        personas.add(new Persona(3, "Joselito", "Jiménez Fernández"));
-        personas.add(new Persona(4, "Laura", "Martínez Ortega"));
-        personas.add(new Persona(5, "María", "Caldel García"));
-        personas.add(new Persona(6, "María del Carmen", "Astero Martín"));
 
-        for (int i=0; i<personas.size(); i++){
-            if(personas.get(i).getId() == id){
-                model.addAttribute("nombre", personas.get(i).getNombre());
-                model.addAttribute("apellidos", personas.get(i).getApellidos());
-            }
-        }
         return "profile";
     }
 
     @GetMapping("/searchPersonal")
-    public ResponseEntity<List<Persona>> buscarPersonas(@RequestParam("nombre") String busqueda) {
+    public ResponseEntity<List<PersonaElastic>> buscarPersonas(@RequestParam("nombre") String busqueda) {
 
-        if(busqueda.length() < 3){
-            List<Persona> emptyList = new ArrayList<>();
-            return ResponseEntity.ok(emptyList);
-        }
-
-        //todo Datos de llegada, son ficticios pero deben de ser los reales.
-        List<Persona> personas = new ArrayList<>();
-
-        personas.add(new Persona(1, "Jose Ángel", "Alcolea Arques"));
-        personas.add(new Persona(2, "Jose Manuel", "Martín Melero"));
-        personas.add(new Persona(3, "Joselito", "Jiménez Fernández"));
-        personas.add(new Persona(4, "Laura", "Martínez Ortega"));
-        personas.add(new Persona(5, "María", "Caldel García"));
-        personas.add(new Persona(6, "María del Carmen", "Astero Martín"));
-
-        List<Persona> personasEncontradas = new ArrayList<>();
-
-        for(int i=0; i<personas.size(); i++){
-            String nombreCompleto = personas.get(i).getNombre() + " " + personas.get(i).getApellidos();
-
-            busqueda = normalize(busqueda).toLowerCase();
-            nombreCompleto = normalize(nombreCompleto).toLowerCase();
-
-            if(nombreCompleto.contains(busqueda)){
-                personasEncontradas.add(personas.get(i));
-            }
-        }
-
+        List<PersonaElastic> personasEncontradas = new ArrayList<>();
         return ResponseEntity.ok(personasEncontradas);
     }
 
     @GetMapping("/searchPublicaciones")
-    public ResponseEntity<List<Publicacion>> buscarPublicaciones(@RequestParam("publicacion") String busqueda) {
+    public ResponseEntity<List<PersonaElastic>> buscarPublicaciones(@RequestParam("publicacion") String busqueda) {
 
-        if(busqueda.length() < 3){
-            List<Publicacion> emptyList = new ArrayList<>();
-            return ResponseEntity.ok(emptyList);
-        }
-
-        //todo Datos de llegada, son ficticios pero deben de ser los reales.
-        List<Publicacion> publicaciones = new ArrayList<>();
-
-        publicaciones.add(new Publicacion(1, "Organizaciones nativas responsables: la RSC en la cultura de las startups digitales españolas", "María Dolores Gil , Margarita Capdepón , Miguel Ángel Beltrán , Francisco José Noguera , Ginesa Martinez , María Dolores García , Jorge López , María Fernández , Victoria Pérez , Mario Cava (2015)"));
-        publicaciones.add(new Publicacion(2, "Propuesta de diseño claro y transparente para la factura eléctrica regulada en España", "Blas José Subiela , María Ascensión Miralles , David Sánchez (2018)"));
-        publicaciones.add(new Publicacion(3, "Influencia del diseño periodístico en la comprensión de la información", "Blas José Subiela , Ariana Gómez (2018)"));
-
-        List<Publicacion> publicacionesEncontradas = new ArrayList<>();
-
-        for (int i=0; i<publicaciones.size(); i++){
-            busqueda = normalize(busqueda).toLowerCase();
-            String titulo = normalize(publicaciones.get(i).getTitulo());
-            String contenido = normalize(publicaciones.get(i).getContenido());
-
-            if(titulo.contains(busqueda) || contenido.contains(busqueda)){
-                publicacionesEncontradas.add(publicaciones.get(i));
-            }
-        }
-
-        return ResponseEntity.ok(publicacionesEncontradas);
+        List<PersonaElastic> personasEncontradas = new ArrayList<>();
+        return ResponseEntity.ok(personasEncontradas);
     }
-
 
     public String normalize(String string) {
         return Normalizer.normalize(string, Normalizer.Form.NFD).replaceAll("\\p{InCombiningDiacriticalMarks}+","");
